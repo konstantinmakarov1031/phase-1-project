e066b7a24de26136c45ec75cc65de0bb842a56dc
konstantin <makarovestates@gmail.com>
2022-08-16 16:18:38 -0500

    Added notes to each function

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-//GET https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/USD  API Key - cf0cb3636d1863b4bab3e6a1
-
-
+//This is one of our addEventListener functions. This function takes the currency input form, waits for a submit event to happen on it (which is the submit button being clicked), 
+//and uses another event listener built inside of the HTML file (the onsubmit) to call the buildConversion function. 
 document.addEventListener('DOMContentLoaded', () => {
     let mainForm = document.querySelector('form')
     mainForm.addEventListener('submit', (e) => {
@@ -11,33 +10,27 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 
+//This function takes our conversionHistory div, and creates an ul inside of a li inside of the div. This div
+//is left empty in the HTML file for this purpose. This in turn makes an easy to iterate over conversion history. We could have left 
+//all of the added elements in this div as p, but that would have made creating a clear button more difficult. 
 function handleCurrencyConversion(currencyConversion){
-    let historyUl = document.getElementById('historyList') ?? document.createElement('ul')    //Remember this and look into it more. 
+    let historyUl = document.getElementById('historyList') ?? document.createElement('ul')  
     historyUl.id = "historyList"
     document.getElementById('conversionHistory').appendChild(historyUl)
     let historyLi = document.createElement('li')
     historyLi.textContent = conversionResult.innerText
-    // historyLi.textContent =  
     historyUl.appendChild(historyLi)
 }
 
 
-// function createHistoryListFlow(historyLi){
-//     historyLi.textContent = `${rate} x ${baseRate} = ${conversionResult.innerText}`
-// }
-
-// createHistoryListFlow(); 
-
-
+//This function simply clears our historyList, and in effect clears the conversion history. 
 function clearHistory() {
     document.getElementById('historyList').remove()
 }
 
 
-
-
-
-
+//This function takes a key value pair as an argument, and takes the dropdown "Convert To" and adds the names of the currencies and stores their rates. Like the handleCurrencyConversion 
+//function, the select is left empty in the HTML file on purpose, to build this inside of the DOM. 
 function addRatesToBox({name, rate}){
     let dropdown = document.getElementById('convertToCurrency')
     let option = document.createElement('option')
@@ -48,30 +41,29 @@ function addRatesToBox({name, rate}){
 }
 
 
+
+//This function takes the event of the dropdown box being selected, and sets the value of each currency. 
 function populateConversionValues(e){
-    e.preventDefault()
-    // console.log(e.target.value) 
+    e.preventDefault() 
     const conversionRate = e.target.value
     const dropDown = document.getElementById('convertToCurrency')
-    // console.log("selector", dropDown.value)
-    // console.log(conversionRate)   
 }
 
 
+//This function builds the conversion itself. Going back to the onsubmit in the HTML file, once the submit button is clicked, it takes the value of the selected currency, and multiplies it 
+//by the value of the base rate. 
 function buildConversion(e){
     e.preventDefault() 
-    const baseRate = e.target.currencyConversion.value
-    // console.log(baseRate) 
+    const baseRate = e.target.currencyConversion.value 
     const dropDownMenu = document.getElementById('convertToCurrency')
-    // console.log(dropDownMenu)
     const rate = dropDownMenu.value 
-    // console.log(rate)
     const conversionResult = document.getElementById('conversionResult')
-    conversionResult.innerText = baseRate * rate 
-    // conversionResult.innerText.reset()
+    conversionResult.innerText = baseRate * rate
 }
 
 
+//This is our fetch function, where we grab all of the information from the API. Furthermore, in this function, we also map over the key value pair of name and rate using Object, 
+//and are thus able to use them separately as opposed to as a pair. 
 function getAllCurrency(){
     fetch('https://v6.exchangerate-api.com/v6/cf0cb3636d1863b4bab3e6a1/latest/USD')
     .then(res => {
@@ -80,19 +72,23 @@ function getAllCurrency(){
 
     .then(currencyList => {
         const {conversion_rates: conversionRates} = currencyList
+        console.log(currencyList)
         const parsedConversionRates = Object.entries(conversionRates)
         const currencyData = parsedConversionRates.map(([name, rate]) => ({name, rate}))
         return currencyData.forEach(rateNamePair => addRatesToBox(rateNamePair))
-        //look at this!
+        
     })   
 }
 
 
+//This is where we set getAllCurrency as our callback function. 
 function initialize(){
     getAllCurrency()
     // currencyList.forEach(rates => addRatesToBox(rates))
 }
 
+
+//This initializes the whole process. 
 initialize();
 
 
